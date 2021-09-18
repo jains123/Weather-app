8574da12f4d0d8ed70c16a2595d27d53b99af9b3
Jamie <jamieainsworth03@gmail.com>
2021-09-18 21:42:23 +0100

    search bar not working

---
+++ script.js
@@ -0,0 +1,31 @@
+let weather = {
+    "apikey": "11de1358fba8f4fab3f7138e1e9fd7f6",
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apikey)
+        .then((Response) => Response.json())
+        .then((data) => this.displayWeather(data));
+    },
+
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+        document.querySelector(".temp").innerHTML = temp + "°C";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind-speed").innerText = "Wind Speed: " + speed + "km/h";
+    },
+
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+      },
+    };
+    
+    document.querySelector(".search button").addEventListener("click", function () {
+      weather.search();
+    });
