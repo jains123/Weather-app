405f81bff50d2bc0531cd9e547d7dd8b6482f65b
Jamie <jamieainsworth03@gmail.com>
2021-09-19 13:25:54 +0100

    finished product

--- script.js
+++ script.js
@@ -11,21 +11,27 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name, icon, description, temp, humidity, speed);
-
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
         document.querySelector(".temp").innerHTML = temp + "°C";
         document.querySelector(".description").innerText = description;
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind-speed").innerText = "Wind Speed: " + speed + "km/h";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
     },
 
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
-      },
-    };
+    },
+};
     
-    document.querySelector(".search button").addEventListener("click", function () {
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
       weather.search();
-    });
+    }
+  });
+
+weather.fetchWeather("Denver");
